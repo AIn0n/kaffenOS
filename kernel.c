a64b065482a6c61a1925e982e1eae189a48e74b5
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2020-10-29 20:53:58 +0100

    nothing done yet

---
+++ kernel.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include_next <stdint.h>
+
+#if defined(__linux__)
+    #error "smth go wrong"
+#elif !defined(__i386__)
+    #error "smth go wrong"
+#endif
+
+volatile uint16_t* vga_buffer = (uint16_t *) 0xB8000;
+const int VGA_COLS = 80;
+const int VGA_ROWS = 25;
+
+int term_col = 0;
+int term_row = 0;
+
+void term_init()
+{
+    for(int col = 0; col < VGA_ROWS; ++col)
+    {
+        for(int row = 0; row < VGA_ROWS; ++row)
+        {
+            //NOT DONE YET
+        }
+    }
+}
+
+int main (void) {return 0;}
