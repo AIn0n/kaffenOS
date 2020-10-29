4f4c71403f0afff258a1f7c46ac881a373e84083
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2020-10-29 21:25:11 +0100

    Repaired boot.s

--- boot.s
+++ boot.s
@@ -3,7 +3,7 @@
 
 .set MB_MAGIC, 0x1BADB002   //magic number for multiboot
 .set MB_FLAGS, (1 << 0) | (1 << 1)
-.set MB_CHECKSUM (0 - (MB_MAGIC - MB_FLAGS))
+.set MB_CHECKSUM, (0 - (MB_MAGIC - MB_FLAGS))
 
 //setting up multiboot header
 .section .multiboot
@@ -18,7 +18,7 @@
         .skip 4096  //reserved 4kb for stack
     stack_top:
 
-.ssection .text
+.section .text
     start:
         mov $stack_top, %esp    //setting up stack
         call kernel_main        //running kernel
