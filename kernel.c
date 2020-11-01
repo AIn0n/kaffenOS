afc1dbbdcff1f2625b66314d270ab33f63c91814
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2020-11-01 23:42:17 +0100

    Repaired char display

--- kernel.c
+++ kernel.c
@@ -13,19 +13,25 @@ const int VGA_COLS = 80;
 const int VGA_ROWS = 25;
 
 //for terminal
-int term_col = 0;
-int term_row = 0;
-uint8_t term_color = 0;
+int term_col;
+int term_row;
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t fg, uint8_t bg)
+{
+    uint16_t color = fg | bg << 4;
+    return (uint16_t) color << 8 | (uint16_t) uc;
+}
 
 void term_init()
 {
-    for(int col = 0; col < VGA_ROWS; ++col)
+    term_col = 0; term_row = 0;
+    for(int col = 0; col < VGA_COLS; ++col)
     {
         for(int row = 0; row < VGA_ROWS; ++row)
         {
             const size_t index = (VGA_COLS * row) + col;
             //VGA_CHAR BBBBFFFFCCCCCCCC, B-background F-foreground C-ASCII char
-            vga_buffer[index] = ((uint16_t) term_color << 8) | ' ';
+            vga_buffer[index] = vga_entry(' ', 0, 0);
             
         }
     }
@@ -38,13 +44,13 @@ void term_putc(char c)
     case '\n':
         {
             term_col = 0;
-            term_row++;
+            ++term_row;
             break;
         }
     default:
         {
             const size_t index = (VGA_COLS * term_row) + term_col;
-            vga_buffer[index] = ((uint16_t) term_color << 8) | c;
+            vga_buffer[index] = vga_entry(c, 15, 0);
             ++term_col;
             break;
         }
@@ -60,5 +66,5 @@ void term_print(const char* str){
 void kernel_main (void) 
 {
     term_init();
-    term_print("kaffenOS\nSMACZNEJ KAWIUSI");
+    term_print("kaffenOS\nNIEDZIELA WIECZOR I HUMOR POPSUTY");
 }
