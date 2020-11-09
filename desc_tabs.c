74e37d78059e61327f07ef3a3025b9a3a6103552
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2020-11-09 19:10:32 +0100

    Working with IDT

--- desc_tabs.c
+++ desc_tabs.c
@@ -5,9 +5,13 @@
 #define LIMIT 0xFFFFFFFF
 
 extern void gdt_flush(uint32_t);
+extern void idt_flush(uint32_t);
 
 gdt_entry_t gdt_entries[5];
 gdt_ptr_t gdt_ptr;
+
+idt_entry_t idt_entries[256];
+idt_ptr_t   idt_ptr; 
 
 static void gdt_set_gate(   uint32_t num,   uint32_t base,
                             uint32_t limit, uint8_t access, uint8_t gran)
@@ -37,6 +41,26 @@ static void init_gdt()
     
     gdt_flush((uint32_t) &gdt_ptr);
 }
+
+uint8_t idt_set_flags(uint8_t present, uint8_t privilage_lvl)
+{
+    uint8_t ret = 14;       //bits between 4-1 are always 0b01110, 0xE, 0d14
+    ret |= privilage_lvl << 5;
+    ret |= (~~(present) << 7);    
+    return ret;
+}
+
+static void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags)
+{
+   idt_entries[num].base_lo = base & 0xFFFF;
+   idt_entries[num].base_hi = (base >> 16) & 0xFFFF;
+
+   idt_entries[num].sel     = sel;
+   idt_entries[num].always0 = 0;
+   // We must uncomment the OR below when we get to using user-mode.
+   // It sets the interrupt gate's privilege level to 3.
+   idt_entries[num].flags   = flags /* | 0x60 */;
+} 
 
 void init_descriptor_tables()
 {
